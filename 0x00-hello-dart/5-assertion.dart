bc79f0caf5080ed6408154d562e8943f015ee8c0
shaw342 <shawanbarua70@gmail.com>
2024-11-07 19:09:18 +0100

    wrong string

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,6 +1,5 @@
 void main(List<String> args) {
 	var argument = int.parse(args[0]);
 	assert(argument >= 80,'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
-	print("You passed");
+	print("You Passed");
 }
-
