4210915aa52648add877832ffcc7b87862f4ce7d
shaw342 <shawanbarua70@gmail.com>
2024-11-19 21:56:25 +0100

    private

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,21 @@
+
+class Password {
+    String _password = "";
+
+
+    Password({String password = ""}){
+      this._password = password;
+    }
+
+    bool isValid(){
+      if (_password.length < 8 || _password.length > 16){
+        return false;
+      }
+      return _password.contains(RegExp(r'[a-z]')) && _password.contains(RegExp(r'[A-Z]')) && _password.contains(RegExp(r'\d'));
+    }
+
+    @override
+  String toString(){
+    return "Your Password is $_password";
+  }
+}
