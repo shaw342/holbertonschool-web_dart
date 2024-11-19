3a8c17f24f3ad4a3a096f5e53f175af299cedbd3
shaw342 <shawanbarua70@gmail.com>
2024-11-19 20:29:50 +0100

    get map

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -3,14 +3,10 @@ class User{
 	int age = 0;
 	double height = 0.0;
 
-	User(String name,int age,double height){
-		this.name = name;
-		this.age = age;
-		this.height = height;
-	}
+	User({required this.name,required this.age,required this.height});
 
-	Map<dynamic,dynamic> toJson(){
-		var result = new Map();
+	Map<String,dynamic> toJson(){
+		var result = new Map<String,dynamic>();
 		result["name"] = this.name;
 		result["age"] = this.age;
 		result["height"] = this.height;
