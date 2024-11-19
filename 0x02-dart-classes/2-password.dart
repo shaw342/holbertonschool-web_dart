a6b0dbf828548bf0a7646947a9cdfbaa7319e42f
shaw342 <shawanbarua70@gmail.com>
2024-11-19 21:12:54 +0100

    password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,16 @@
+
+class Password {
+    String password = "";
+
+    bool isValid(){
+      if (password.length < 8 || password.length > 16){
+        return false;
+      }
+      return password.contains(RegExp(r'[a-z]')) && password.contains(RegExp(r'[A-Z]')) && password.contains(RegExp(r'\d'));
+    }
+
+    @override
+  String toString(){
+    return "Your password is $password";
+  }
+}
