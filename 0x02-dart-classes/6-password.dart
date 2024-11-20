8ad1310a2f18ccff5645fd9b1c3ea437ebdeb4c2
shaw342 <shawanbarua70@gmail.com>
2024-11-20 17:43:56 +0100

    inheritance task 6

---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,30 @@
+
+class Password {
+    String _password = "";
+
+
+
+    String get password{
+      return password;
+    }
+
+    void set password (String password) {
+      this._password = password;
+    }
+    
+    Password({String password = ""}){
+      this._password = password;
+    }
+
+    bool isValid(){
+      if (_password.length < 8 || _password.length > 16){
+        return false;
+      }
+      return _password.contains(RegExp(r'[a-z]')) && _password.contains(RegExp(r'[A-Z]')) && _password.contains(RegExp(r'\d'));
+    }
+
+    @override
+  String toString(){
+    return "Your Password is: $_password";
+  }
+}
